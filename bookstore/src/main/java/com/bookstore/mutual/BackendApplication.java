46bdf5aa09c9b1dce0b1544490733927829421f6
Lacina ZINA <zinalacina@gmail.com>
2019-11-04 07:28:44 -0600

    Put JWT to 5 minutes

--- bookstore/src/main/java/com/bookstore/mutual/BackendApplication.java
+++ bookstore/src/main/java/com/bookstore/mutual/BackendApplication.java
@@ -2,11 +2,9 @@ package com.bookstore.mutual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableMongoAuditing
